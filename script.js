dcf837d1998ee2fa0d3020a4c513e6f4ae3807a2
Mar-Nb <niombela.martin@gmail.com>
2023-06-28 10:36:37 +0200

    Faux fetch + affichage résultat

--- script.js
+++ script.js
@@ -21,13 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
 // Pour pouvoir afficher les modals de la page (Panier et Commandes)
 document.addEventListener("DOMContentLoaded", () => {
   // Functions to open and close a modal
-  function openModal($el) {
-    $el.classList.add("is-active");
-  }
+  function openModal($el) { $el.classList.add("is-active"); }
 
-  function closeModal($el) {
-    $el.classList.remove("is-active");
-  }
+  function closeModal($el) { $el.classList.remove("is-active"); }
 
   function closeAllModals() {
     (document.querySelectorAll(".modal") || []).forEach(($modal) => {
@@ -42,18 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const modal = $trigger.dataset.target;
     const $target = document.getElementById(modal);
 
-    $trigger.addEventListener('click', () => {
-      openModal($target);
-    });
+    $trigger.addEventListener('click', () => { openModal($target); });
   });
 
   // Add a click event on various child elements to close the parent modal
   (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
     const $target = $close.closest('.modal');
 
-    $close.addEventListener('click', () => {
-      closeModal($target);
-    });
+    $close.addEventListener('click', () => { closeModal($target); });
   });
 
   // Add a keyboard event to close all modals
@@ -66,13 +58,19 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function generateArticle() {
-  document.querySelector("#liste-result").innerHTML = "<h1 class='title'>Résultat de la recherche</h2>";
+// *******************************************************************************************************
+
+const urlFetch = "./trips.json";
+const imgHolder = document.querySelector("#img-holder");
+const listeResult = document.querySelector("#liste-result");
+const resultZone = document.querySelector("#result-zone");
+
+function generateArticle(trip) {
   const articleTrip = `<article><div class="columns">
     <div class="column is-1"><span class="icon"><i class="fa-solid fa-train"></i></span></div>
-    <div class="column"><p class="has-text-weight-bold">Test > Test</p></div>
-    <div class="column"><p>00h00</p></div>
-    <div class="column"><p>999€</p></div>
+    <div class="column"><p class="has-text-weight-bold">${trip.departure} > ${trip.arrival}</p></div>
+    <div class="column"><p>${new Date(trip.date.$date).getHours()}h${new Date(trip.date.$date).getMinutes()}</p></div>
+    <div class="column"><p>${trip.price}€</p></div>
     <div class="column is-1">
       <div class="field">
         <div class="control">
@@ -83,4 +81,41 @@ function generateArticle() {
       </div>    
     </div>
   </div></article>`;
+
+  listeResult.innerHTML += articleTrip;
 }
+
+// Action de recherche de voyage
+document.querySelector("#search-btn").addEventListener("click", function() {
+  const searchData = {
+    dep: document.querySelector("#field-depart").value,
+    arr: document.querySelector("#field-arrive").value,
+    date: document.querySelector("#field-date").value
+  };
+  
+  // console.log(date);
+
+  fetch(urlFetch)
+    .then(response => response.json())
+    .then(trips => {
+      const dummy = { result: true, trips };
+      if (! dummy.result) {
+        imgHolder.querySelector("img").src = "./images/into_the_night_empty.svg";
+        imgHolder.querySelector("p").textContent = "Désolé, pas de destination pour votre demande...";
+      } else {
+        listeResult.classList.remove("is-hidden");
+        imgHolder.classList.add("is-hidden");
+        resultZone.querySelector("h2").classList.remove("is-hidden");
+        listeResult.innerHTML = "";
+
+        for (let trip of dummy.trips) {
+          // console.log(trip.date.$date);
+          const formatDate = (new Date(trip.date.$date)).toISOString().split('T')[0];
+          if (formatDate === searchData.date && trip.departure === searchData.dep && trip.arrival === searchData.arr) {
+            generateArticle(trip);
+          }
+        }
+      }
+    });
+
+});
